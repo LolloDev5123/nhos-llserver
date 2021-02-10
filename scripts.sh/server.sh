806d86d2fb5ba3b88b8827ee44b1aa5955081912
totakaro <30659361+totakaro@users.noreply.github.com>
2021-02-10 12:43:30 -0500

    Fix server.sh on boot
    Makes sure server.sh executes in background

--- scripts.sh/server.sh
+++ scripts.sh/server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sleep 1
-echo "`date` Local Log Sever is running..."
-
 while true 1; do
   echo -ne "HTTP/1.0 200 OK\r\n\r\n `cat /mnt/nhos/index.html` `tail /var/log/nhos/nhm/miners/*.log /var/log/nhos/*.log` </textarea></body></html>" | nc -l -p 8080 > /tmp/server.log;
   # cat /tmp/server.log # debug only
@@ -10,4 +7,4 @@ while true 1; do
   if [ "${?}" -eq 0 ]; then
     sudo reboot
   fi
-done
+done&
